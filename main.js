77faaa7f90ba771b361da993f4d8834db81c9c86
Qianglong Mo <sammok2003@hotmail.com>
2015-06-24 13:55:29 +0800

    Add weixin share image.

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var idList = [];
+
+for (var i = 1; i <= 150; i++) {
+    var id = i;
+
+    if ((i+"i").indexOf('4') == -1) {
+        if (id < 10) {
+            id = '00' + i;
+        }
+        else if (id < 100) {
+            id = "0" + i;
+        }
+
+        idList.push('888' + id);
+    }
+
+    console.dir(idList);
+}
